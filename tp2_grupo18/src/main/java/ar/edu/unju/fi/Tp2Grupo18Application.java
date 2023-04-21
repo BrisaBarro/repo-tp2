2066628b07ed453a1e2413d9922a588198fb439b
BRISA.BARRO <BRISA.BARRO@BRISA>
2023-04-20 22:54:07 -0300

    Proyecto tp2 Inicial

---
+++ tp2_grupo18/src/main/java/ar/edu/unju/fi/Tp2Grupo18Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo18Application.class, args);
+	}
+
+}
